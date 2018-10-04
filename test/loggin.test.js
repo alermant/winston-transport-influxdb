98332eb46982fd656944e0c65981b40b4e0a6796
Alexandre Lermant <alermant.dev@outlook.fr>
2018-10-04 20:38:29 +0200

    feature/tests-winston | add test using winston

--- test/loggin.test.js
+++ test/loggin.test.js
@@ -1,8 +1,17 @@
 const Transport = require('../index');
+const { createLogger  } = require('winston');
 
 describe('transport', function () {
   it('should log', function () {
     const transport = new Transport();
     transport.log({level: 'info', message: 'test'}, () => {});
   });
+
+  it('should log using winston', function () {
+    const logger = createLogger({
+      transports: [new Transport()]
+    });
+    logger.log({level: 'info', message: 'message'});
+    logger.warn({"message": "my message"});
+  })
 });
